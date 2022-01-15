da94eb6e8b84a0f403bf0a3c9f74528ee0430769
allacee <ddimaglushkov@ya.ru>
2022-01-15 16:30:40 +0300

    Add ip address block on alter panel

--- config.h
+++ config.h
@@ -13,15 +13,17 @@ typedef struct {
 
 static Block blocks[] = {
 	// CMD					INTERV	SIGNAL	VIS		GRAPH
-	{ "sb-connection", 		10, 	0,		1,		0 },
+	{ "sb-ip-address", 		0,		20,		0,		0 },
+	{ "sb-connection", 		10, 	20,		1,		0 },
 	{ "sb-weather", 		3600, 	19,		1,		0 },
 	{ "sb-graph-cpu", 		2, 		18, 	1,		1 },
 	{ "sb-graph-mem", 		2, 		17, 	1,		1 },
-	{ "sb-gpu-mode", 		0, 		16,		0,		0 },
-	{ "sb-jupyter", 		5, 		15,		0,		0 },
-	{ "sb-brightness", 		0, 		14,		1,		0 },
-	{ "sb-volume", 			0, 		13,		1,		0 },
-	{ "sb-battery", 		5, 		12,		1,		0 },
+	{ "sb-brightness", 		10,		16,		1,		0 },
+	{ "sb-volume", 			10,		15,		1,		0 },
+	{ "sb-battery", 		5, 		14,		1,		0 },
+	{ "sb-cp-webcam", 		5, 		0,		0,		0 },
+	{ "sb-cp-jupyter", 		5, 		13,		0,		0 },
+	{ "sb-cp-gpu", 			0, 		12,		0,		0 },
 	{ "sb-datetime", 		1, 		11,		1,		0 },
 };
 
